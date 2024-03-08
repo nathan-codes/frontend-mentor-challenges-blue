6ed2271424735a18544a36825e4e6f72754d4bb5
Nathan Turkson <nathanturkson@gmail.com>
2024-03-08 16:38:55 +0000

    progress on article preview card

--- article-preview-component/src/app/page.tsx
+++ article-preview-component/src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
+// Manrope Google Font used
+import { manrope } from "./fonts";
+import ArticlePreviewCard from "@/components/ArticlePreviewCard";
+
+
 export default function Home() {
   return (
-    <main className="">
-        <p>lorem 23</p>
+    <main
+      className={`${manrope.className}  flex justify-center items-center min-h-screen bg-lightGrayishBlue`}
+    >
+      <ArticlePreviewCard />
     </main>
   );
 }
